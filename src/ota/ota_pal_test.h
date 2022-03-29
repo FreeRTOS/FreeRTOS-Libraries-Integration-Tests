19f78e331bcbb8b3cbe0dbb58305bcd19a0e21bc
Tianming Chen <tianmc@amazon.com>
2022-03-29 16:03:16 -0700

    Removed unused param in OTA PAL test

--- src/ota/ota_pal_test.h
+++ src/ota/ota_pal_test.h
@@ -1,11 +1,8 @@
 #ifndef _OTA_PAL_TEST_H_
 #define _OTA_PAL_TEST_H_
 
-typedef void (* MemFreeFunc)( void *ptr );
-
 typedef struct OtaPalTestParam
 {
-    MemFreeFunc pMemFree;
 } OtaPalTestParam_t;
 
 void SetupOtaPalTestParam( OtaPalTestParam_t * pTestParam );
