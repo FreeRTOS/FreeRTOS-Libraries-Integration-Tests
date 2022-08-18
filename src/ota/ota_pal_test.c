cb63e36edc35eccef0b93c14c91d3fae74f1841e
ActoryOu <ousc@amazon.com>
2022-08-18 16:30:44 +0800

    Fix OTA PAL test with LTS 2.0 libraries.

--- src/ota/ota_pal_test.c
+++ src/ota/ota_pal_test.c
@@ -131,7 +131,7 @@ TEST_GROUP_RUNNER( Full_OTA_PAL )
 TEST( Full_OTA_PAL, otaPal_CloseFile_ValidSignature )
 {
     OtaPalStatus_t xOtaStatus;
-    Sig256_t xSig = { 0 };
+    Sig_t xSig = { 0 };
     int16_t bytesWritten;
 
     /* We use a dummy file name here because closing the system designated bootable
@@ -169,7 +169,7 @@ TEST( Full_OTA_PAL, otaPal_CloseFile_ValidSignature )
 TEST( Full_OTA_PAL, otaPal_CloseFile_InvalidSignatureBlockWritten )
 {
     OtaPalStatus_t xOtaStatus;
-    Sig256_t xSig = { 0 };
+    Sig_t xSig = { 0 };
     int16_t bytesWritten;
 
     /* Create a local file using the PAL. */
@@ -209,7 +209,7 @@ TEST( Full_OTA_PAL, otaPal_CloseFile_InvalidSignatureBlockWritten )
 TEST( Full_OTA_PAL, otaPal_CloseFile_InvalidSignatureNoBlockWritten )
 {
     OtaPalStatus_t xOtaStatus;
-    Sig256_t xSig = { 0 };
+    Sig_t xSig = { 0 };
 
     /* Create a local file using the PAL. */
     xOtaFile.pFilePath = ( uint8_t * ) OTA_PAL_FIRMWARE_FILE;
@@ -572,7 +572,7 @@ TEST( Full_OTA_PAL, otaPal_SetPlatformImageState_AbortImageState )
 TEST( Full_OTA_PAL, otaPal_GetPlatformImageState_InvalidImageStateFromFileCloseFailure )
 {
     OtaPalStatus_t xOtaStatus;
-    Sig256_t xSig = { 0 };
+    Sig_t xSig = { 0 };
     OtaPalImageState_t ePalImageState = OtaPalImageStateUnknown;
     int16_t bytesWritten;
 
