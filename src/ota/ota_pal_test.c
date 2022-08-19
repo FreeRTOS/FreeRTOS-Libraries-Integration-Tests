cbc1549f6c1b88e5077e2b71ced84821f6163f67
ActoryOu <ousc@amazon.com>
2022-08-19 12:30:55 +0800

    Set version as INT.

--- src/ota/ota_pal_test.c
+++ src/ota/ota_pal_test.c
@@ -137,11 +137,11 @@ TEST_GROUP_RUNNER( Full_OTA_PAL )
 TEST( Full_OTA_PAL, otaPal_CloseFile_ValidSignature )
 {
     OtaPalStatus_t xOtaStatus;
-    #if OTA_PAL_TEST_OTA_LIBRARY_VERSION == "v3.3.0"
+    #if OTA_PAL_TEST_OTA_LIBRARY_VERSION <= ( 330 )
         Sig256_t xSig = { 0 };
     #else
         Sig_t xSig = { 0 };
-    #endif /* OTA_PAL_TEST_OTA_LIBRARY_VERSION == "v3.3.0" */
+    #endif /* OTA_PAL_TEST_OTA_LIBRARY_VERSION <= ( 330 ) */
     int16_t bytesWritten;
 
     /* We use a dummy file name here because closing the system designated bootable
@@ -179,11 +179,11 @@ TEST( Full_OTA_PAL, otaPal_CloseFile_ValidSignature )
 TEST( Full_OTA_PAL, otaPal_CloseFile_InvalidSignatureBlockWritten )
 {
     OtaPalStatus_t xOtaStatus;
-    #if OTA_PAL_TEST_OTA_LIBRARY_VERSION == "v3.3.0"
+    #if OTA_PAL_TEST_OTA_LIBRARY_VERSION <= ( 330 )
         Sig256_t xSig = { 0 };
     #else
         Sig_t xSig = { 0 };
-    #endif /* OTA_PAL_TEST_OTA_LIBRARY_VERSION == "v3.3.0" */
+    #endif /* OTA_PAL_TEST_OTA_LIBRARY_VERSION <= ( 330 ) */
     int16_t bytesWritten;
 
     /* Create a local file using the PAL. */
@@ -223,11 +223,11 @@ TEST( Full_OTA_PAL, otaPal_CloseFile_InvalidSignatureBlockWritten )
 TEST( Full_OTA_PAL, otaPal_CloseFile_InvalidSignatureNoBlockWritten )
 {
     OtaPalStatus_t xOtaStatus;
-    #if OTA_PAL_TEST_OTA_LIBRARY_VERSION == "v3.3.0"
+    #if OTA_PAL_TEST_OTA_LIBRARY_VERSION <= ( 330 )
         Sig256_t xSig = { 0 };
     #else
         Sig_t xSig = { 0 };
-    #endif /* OTA_PAL_TEST_OTA_LIBRARY_VERSION == "v3.3.0" */
+    #endif /* OTA_PAL_TEST_OTA_LIBRARY_VERSION <= ( 330 ) */
 
     /* Create a local file using the PAL. */
     xOtaFile.pFilePath = ( uint8_t * ) OTA_PAL_FIRMWARE_FILE;
@@ -590,11 +590,11 @@ TEST( Full_OTA_PAL, otaPal_SetPlatformImageState_AbortImageState )
 TEST( Full_OTA_PAL, otaPal_GetPlatformImageState_InvalidImageStateFromFileCloseFailure )
 {
     OtaPalStatus_t xOtaStatus;
-    #if OTA_PAL_TEST_OTA_LIBRARY_VERSION == "v3.3.0"
+    #if OTA_PAL_TEST_OTA_LIBRARY_VERSION <= ( 330 )
         Sig256_t xSig = { 0 };
     #else
         Sig_t xSig = { 0 };
-    #endif /* OTA_PAL_TEST_OTA_LIBRARY_VERSION == "v3.3.0" */
+    #endif /* OTA_PAL_TEST_OTA_LIBRARY_VERSION <= ( 330 ) */
     OtaPalImageState_t ePalImageState = OtaPalImageStateUnknown;
     int16_t bytesWritten;
 
